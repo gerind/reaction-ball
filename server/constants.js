8ccbddd5219f94b855d00e0dc87e4da2f901565e
gerind <gerind@yandex.ru>
2022-07-25 10:50:42 +0300

    less server generation

--- server/constants.js
+++ server/constants.js
@@ -3,7 +3,7 @@ module.exports.WIDTH = 1366
 module.exports.HEIGHT = 768
 
 module.exports.FPS = 60
-module.exports.SECOND_GENERATE = 600
+module.exports.SECOND_GENERATE = 240
 module.exports.RADIUS = 8
 module.exports.STORE_TIME = 720000
 
