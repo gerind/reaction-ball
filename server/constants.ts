1554ee2be279d6687a7661fc31daa3fa873ae20c
gerind <gerind@yandex.ru>
2022-12-14 15:12:58 +0500

    fix

--- server/constants.ts
+++ server/constants.ts
@@ -2,6 +2,6 @@ export const WIDTH = 1366
 export const HEIGHT = 768
 
 export const FPS = 60
-export const SECOND_GENERATE = 300
+export const SECOND_GENERATE = 180
 export const RADIUS = 8
 export const STORE_TIME = 720000
